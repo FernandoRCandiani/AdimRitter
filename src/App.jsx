71a82d13c4ea933ba3452a20eb068c9acfc13b9b
Carlos Faustino <chfcchfc96@gmail.com>
2023-09-13 22:26:15 -0300

    fetch refresh token

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { Rotas } from "./routes";
+import { fetchRefreshToken } from './services/fetches';
 
 function App() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchRefreshToken()
+        .then(response => setToken(response.token));
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <BrowserRouter>
       <Rotas />
