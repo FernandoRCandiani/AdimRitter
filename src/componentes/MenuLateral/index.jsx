ba573882e42e7c0025a5f7e406beda72b475d4ca
Fernando Candiani <79520984+FernandoRCandiani@users.noreply.github.com>
2023-09-14 19:35:35 -0300

    Ajuste nos botões lateral

--- src/componentes/MenuLateral/index.jsx
+++ src/componentes/MenuLateral/index.jsx
@@ -5,20 +5,22 @@ import { Empresa } from "../../pages/Empresa";
 import "./style.css";
 
 export function MenuLateral() {
+  const location = useLocation();
+  
   return (
     <aside className="d-flex justify-content-start side-menu">
       <div className="shadow m-0">
         <div className="row gap-2 m-0">
 
-          <Link to="/dashboard" className="btn-outline-focus p-2">
+          <Link to="/dashboard" className={["btn-outline-focus", "p-2", location.pathname === '/dashboard' ? 'active' : ''].join(" ")}>
             Dashboard
           </Link>
 
-          <Link to="/usuario" className="btn-outline-focus p-2">
+          <Link to="/usuario" className={["btn-outline-focus", "p-2", location.pathname === '/usuario' ? 'active' : ''].join(" ")}>
             Usuários
           </Link>
 
-          <Link to="/empresa" className="btn-outline-focus p-2">
+          <Link to="/empresa" className={["btn-outline-focus", "p-2", location.pathname === '/empresa' ? 'active' : ''].join(" ")}>
             Empresas
           </Link>
         </div>
