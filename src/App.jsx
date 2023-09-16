304b9dda5dea6b76cc2e6b0010d6ed4113f1ca4a
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-09-16 15:43:02 -0300

    remove refresh token

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { GlobalProvider } from "./contexts/Global";
 import { Rotas } from "./routes";
-import { fetchRefreshToken } from './services/fetches';
 
 function App() {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetchRefreshToken()
-        .then(response => setToken(response.token));
-    }, 60000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <GlobalProvider>
       <BrowserRouter>
