f05b96eb7409d88830825cd32d42ca77ac9a82bb
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-09-16 18:59:06 -0300

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import { GlobalProvider } from "./contexts/Global";
 import { Rotas } from "./routes";
+import { queryClient } from "./services/queryClient";
 
 function App() {
   return (
-    <GlobalProvider>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </GlobalProvider>
+    <QueryClientProvider client={queryClient}>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Rotas />
+        </BrowserRouter>
+      </GlobalProvider>
+    </QueryClientProvider>
   );
 }
 
