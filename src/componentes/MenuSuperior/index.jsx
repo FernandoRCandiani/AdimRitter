e6b894c4c97a200d065a2d734be555eb30755802
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-09-16 15:45:00 -0300

    connect logged user

--- src/componentes/MenuSuperior/index.jsx
+++ src/componentes/MenuSuperior/index.jsx
@@ -1,6 +1,19 @@
-import './style.css'
+import { useGlobal } from "../../contexts/Global";
+import { getUser, signout } from "../../services/auth";
+
+import './style.css';
 
 export function MenuSuperior() {
+  const { handleLoader } = useGlobal();
+
+  const user = getUser();
+
+  function logout() {
+    handleLoader(true);
+
+    signout();
+  }
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary shadow head">
       <div className="container-fluid justify-content-between">
@@ -11,18 +24,18 @@ export function MenuSuperior() {
           height="70"
         />
 
-        <a className="d-flex navbar-brand align-items-center" href="#">
+        <div className="d-flex navbar-brand align-items-center">
           <img
-            src="/icon.png"
-            alt="Foto de perfil"
+            src={user?.image ?? "/icon.png"}
+            alt={user.name}
             width="70"
             height="70"
-            className="d-inline-block align-text-top rounded-circle me-3"
+            className="d-inline-block align-text-top rounded-circle me-3 profile-image"
           />
-          Olá, Ritter Humboldt
-        </a>
+          Olá, {user.name ?? "Ritter Humboldt"}
+        </div>
 
-        <button className="btn btn-outline-danger" type="submit">
+        <button className="btn btn-outline-danger" onClick={logout}>
           Sair
         </button>
       </div>
