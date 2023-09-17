6dd767018a685b575c5ec530358bc04de8376011
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-09-17 17:12:43 -0300

    change height and accept files

--- src/componentes/Dropzone/index.jsx
+++ src/componentes/Dropzone/index.jsx
@@ -1,43 +1,44 @@
-import React, { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
-import { FiUpload } from 'react-icons/fi'
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
 
-import './style.css'
+import './style.css';
 
 const Dropzone = ({ onFileUploaded }) => {
-   const [selectFile, setSelectFile] = useState('')
+  const [selectFile, setSelectFile] = useState('');
 
-   const onDrop = useCallback(acceptFiles => {
-      //ACEITAR APENAS UM ARQUIVO
-      const file = acceptFiles[0]
+  const onDrop = useCallback(acceptFiles => {
+    const file = acceptFiles[0];
 
-      const fileUrl = URL.createObjectURL(file)
+    const fileUrl = URL.createObjectURL(file);
 
-      setSelectFile(fileUrl)
-      onFileUploaded(file)
-   }, [onFileUploaded])
+    setSelectFile(fileUrl);
+    onFileUploaded(file);
+  }, [onFileUploaded]);
 
-   const { getRootProps, getInputProps } = useDropzone({
-      onDrop,
-      accept: "image/*"
-   })
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/jpg': [".jpg", ".jpeg"],
+      'image/png': [".png"],
+      'image/svg': [".svg"],
+    }
+  });
 
-   return (
-      <div {...getRootProps()} className="dropzone ">
-         <input {...getInputProps()} accept="image/*" />
+  return (
+    <div {...getRootProps()} className="dropzone">
+      <input {...getInputProps()} />
 
-         {
-            selectFile
-               ? <img src={selectFile} alt="Ponto de coleta" />
-               : (
-                  <p>
-                     <FiUpload />
-                     Adicionar Imagem
-                  </p>
-               )
-         }
-      </div>
-   )
-}
+      {selectFile
+        ? <img src={selectFile} alt="Ponto de coleta" />
+        : (
+          <p>
+            <FiUpload />
+            Adicionar Imagem
+          </p>
+        )}
+    </div>
+  );
+};
 
-export default Dropzone
+export default Dropzone;
