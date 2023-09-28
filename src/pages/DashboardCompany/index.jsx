93b3cfa73df407769657a26b1619480dbaa7ca7e
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-09-28 16:05:45 -0300

    add import

--- src/pages/DashboardCompany/index.jsx
+++ src/pages/DashboardCompany/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { PieChart, Pie, Legend, Cell, BarChart, Bar, XAxis, LabelList, YAxis, Tooltip, CartesianGrid, Sector } from "recharts";
+import { PieChart, Pie, Legend, Cell, BarChart, Bar, XAxis, LabelList, YAxis, Tooltip, CartesianGrid, Sector, Text } from "recharts";
 
 import { Dash } from "../../componentes/Dash";
 
@@ -84,16 +84,25 @@ export function DashboardCompany() {
     );
   };
 
+  const CustomTick = (props) => {
+    const { x, y, payload } = props;
+
+    return (
+      <Text x={x + 5} y={y + 25} width={10} textAnchor="middle" fill="#666" fontSize={12} angle={-35} orientation={"top"}>
+        {payload.value}
+      </Text>
+    );
+  };
+
   return (
     <div className="row">
       <div className="col-12">
         <div className="dashboard">
           <Dash title="Ranking geral">
-            <BarChart data={generalRanking} width={600} height={350} >
-              <Legend />
+            <BarChart data={generalRanking} width={600} height={400} margin={{ top: 20, bottom: 40 }}>
               <CartesianGrid strokeDasharray="3 3" stroke="#aaa" />
-              <XAxis dataKey="name" fontSize={12} interval={0} stroke="#222" />
-              <YAxis fontSize={13} allowDataOverflow allowDecimals />
+              <XAxis dataKey="name" fontSize={12} interval={0} label={{ value: "Usuários", dy: 40, fill: "#000" }} stroke="#222" tick={<CustomTick />} />
+              <YAxis fontSize={13} allowDataOverflow dataKey={v => parseInt(v.level)} type="number" label={{ value: "Nível", angle: -90, fill: "#000" }} />
               <Tooltip position={{ y: 0 }} />
               <Bar barSize={40} name="Nível" dataKey="level" fill="#007CC3">
                 <LabelList position="inside" offset={30} angle={90} fill="#fff" fontSize={13} />
@@ -102,32 +111,32 @@ export function DashboardCompany() {
           </Dash>
 
           <Dash title="Total de certificados emitidos">
-            <PieChart width={700} height={350}>
+            <PieChart width={700} height={400} margin={{ top: 20, bottom: 40 }}>
               <Pie data={userCertificates}
                 nameKey="name"
                 dataKey="qtde"
                 activeIndex={activeIndex}
                 activeShape={renderActiveShape}
-                cy={150}
-                cx={350}
+                cy={175}
+                cx={250}
                 innerRadius={50}
                 outerRadius={100}
                 paddingAngle={4}
                 cornerRadius={4}
                 onMouseEnter={(_, i) => setActiveIndex(i)}
               >
-                {userCertificates.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)}
+                {userCertificates.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)}
               </Pie>
               <Legend align="right" layout="vertical" verticalAlign="middle" content={LegendWithValues} />
             </PieChart>
           </Dash>
 
           <Dash title="Média de pontos por missão">
-            <BarChart data={avgMissionGeneral} width={600} height={350}>
+            <BarChart data={avgMissionGeneral} width={600} height={400} margin={{ top: 20, bottom: 40 }}>
               <Legend />
               <CartesianGrid strokeDasharray="3 3" stroke="#aaa" />
               <XAxis dataKey={"name"} fontSize={12} interval={0} stroke="#222" />
-              <YAxis fontSize={13} allowDataOverflow allowDecimals domain={[0, 100]} />
+              <YAxis fontSize={12} allowDecimals dataKey={v => parseInt(v.avg)} type="number" domain={[0, 100]} />
               <Tooltip position={{ y: 0 }} />
               <Bar barSize={40} name="Média" dataKey="avg" isAnimationActive={false} fill="#377B2B">
                 <LabelList position="inside" offset={30} angle={90} fill="#fff" fontSize={13} />
@@ -136,11 +145,11 @@ export function DashboardCompany() {
           </Dash>
 
           <Dash title="Total de problemas resolvidos">
-            <BarChart data={avgTroubleCompanies} width={600} height={350} >
+            <BarChart data={avgTroubleCompanies} width={600} height={400} margin={{ top: 20, bottom: 40 }}>
               <Legend />
               <CartesianGrid strokeDasharray="3 3" stroke="#aaa" />
               <XAxis dataKey="name" fontSize={12} interval={0} stroke="#222" />
-              <YAxis fontSize={13} allowDataOverflow allowDecimals />
+              <YAxis fontSize={12} dataKey={v => parseInt(v.qtde)} type="number" />
               <Tooltip position={{ y: 0 }} />
               <Bar barSize={40} name="Quantidade" dataKey="qtde" isAnimationActive={false} fill="#6251A3">
                 <LabelList position="inside" offset={30} angle={90} fill="#fff" fontSize={13} />
